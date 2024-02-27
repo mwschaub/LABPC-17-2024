71191a98cb3d59f4ba5aca1c6d11e24a856780f8
mwschaub <157499371+mwschaub@users.noreply.github.com>
2024-02-27 10:12:17 -0600

    Add files via upload

---
+++ semana-7 T1 y T2/T1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace T1MWSG1243424{
+    class Program
+    {
+
+        static void Main (string [] args)
+        {
+        Console.WriteLine("Mi segundo programa");
+        string sNombre = "";
+        int sEdad = 0;
+        string sCarrera = "";
+        int sCarne = 0;
+        Console.WriteLine("Ingrese nombre");
+        sNombre = Console.ReadLine();
+        Console.WriteLine("Ingrese edad");
+        sEdad = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese carrera");
+        sCarrera = Console.ReadLine();
+        Console.WriteLine("Ingrese carne");
+        sCarne = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("Mi segundo programa \n Nombre: " + sNombre + "\n Edad: "+sEdad+"\n Carrera: " + sCarrera+ "\n Carné: "+sCarne);
+        Console.WriteLine("Soy " + sNombre + " tengo "+sEdad+" años y estudio la carrera de " + sCarrera+ " Mi numero de carné es: "+sCarne);
+        Console.ReadKey();
+        }
+    }
+}
